55b45c16b320ab022662b584a5f1194a9f8ed935
Joshua Tsang <joshua.tsang@soundmouse.com>
2024-07-06 18:48:30 +0100

    added a working yolov8 script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # basketball_highlight_generator_v1
 Generate basketball highlights automatically
+
+# Setup the venv
+
+$ python3 -m venv venv
+
+# Pip install required packages
+
+$ pip install ultralytics
+
+# Cutting videos down
+
+$ ffmpeg -ss 00:10:00 -t 00:05:00 -i C0012.MP4 -c:v copy C0012_5mins.MP4
--- try_run_yolov.py
+++ try_run_yolov.py
@@ -1,1 +1,16 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+
+    # Load a model
+    model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+    # Use the model
+    model.train(data="coco8.yaml", epochs=3)  # train the model
+    metrics = model.val()  # evaluate model performance on the validation set
+    #results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+    results = model("C0012_5mins.MP4")
+
+    path = model.export(format="onnx")  # export the model to ONNX format
 
